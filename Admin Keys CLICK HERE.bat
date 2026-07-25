3e9c933fc32a711a8d8d6f0f7b506764dae8db7d
Roulette Admin <admin@roulette.local>
2026-07-25 02:12:20 -0700

    Production release code

--- Admin Keys CLICK HERE.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title Admin Key Manager
-echo Starting Admin Dashboard Desktop App...
-cd /d "%~dp0"
-.venv\Scripts\python.exe admin_app\main.py
--- Roulette APP CLICK here.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title TikTok Roulette Wheel Client
-echo Starting Roulette Desktop App...
-cd /d "%~dp0"
-.venv\Scripts\python.exe roulette_app\main.py
